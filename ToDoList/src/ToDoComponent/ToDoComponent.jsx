cef2f2701718bb909942530f49a9aee451824b8b
Sonika Kumari <sonikakumaribom@gmail.com>
2025-02-04 12:44:22 +0000

    implemented dispaly each tak in a list along with check and delete react icons

--- ToDoList/src/ToDoComponent/ToDoComponent.jsx
+++ ToDoList/src/ToDoComponent/ToDoComponent.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './ToDoComponent.css'
 import { useEffect } from 'react';
+import { FaCheckCircle } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 export const TodoComponent =()=>{
     const [inputValue,setInputValue] = useState("")
     const [task, setTask] =useState([]);
@@ -24,6 +26,7 @@ export const TodoComponent =()=>{
             
         }) //state update is asynchronus, but doesnt return a promise. hence use the useEffect hook to log updated value of task.
         //console.log(task) //simple log statement returns previous value as the setTask is async
+        setInputValue("")
     }
     return(
         <section className="todo-container">
@@ -37,6 +40,24 @@ export const TodoComponent =()=>{
                     ></input>
                     <button type='submit'>Add Task</button>
                 </form>
+            </section>
+            <section>
+                <ul className='todo-list'>
+                    {
+                        task.map((currTask,index)=>{
+                            return (
+                            <li key={index} className='todo-item'>
+                                <span>{currTask}</span>
+                                <div>
+                                <button className='check-btn'><FaCheckCircle /></button>
+                                <button className='delete-btn'><MdDeleteForever /></button>
+                                </div>
+                                
+                            </li>
+                            )
+                        })
+                    }
+                </ul>
             </section>
         </section>
     )
